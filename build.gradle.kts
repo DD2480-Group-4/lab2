f763532d88a1129d4928217cf2f9daa3311ecccd
Erik Winbladh <erik.j.winbladh@gmail.com>
2024-02-05 14:48:28 +0100

    init: Code Skeleton (#2)
    * Add code skeleton for mandatory functionality
    
    ---------
    
    Co-authored-by: Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
    Co-authored-by: Douglas Fischer <anakin_skryter@hotmail.se>
    Co-authored-by: Robin Claesson <robin.claesson@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+	id("java")
+}
+
+group = "group4"
+version = "1.0-SNAPSHOT"
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// https://mvnrepository.com/artifact/javax.servlet/javax.servlet-api
+	compileOnly("javax.servlet:javax.servlet-api:3.0.1")
+	// https://mvnrepository.com/artifact/org.eclipse.jetty/jetty-servlet
+	implementation("org.eclipse.jetty:jetty-servlet:7.0.2.v20100331")
+
+	testImplementation(platform("org.junit:junit-bom:5.9.1"))
+	testImplementation("org.junit.jupiter:junit-jupiter")
+	testImplementation("org.assertj:assertj-core:3.25.1")
+}
+
+tasks.test {
+	useJUnitPlatform()
+}
