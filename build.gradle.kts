c443552dfa11e3214a0ffb46e31af3411714182d
ractodev <erik.j.winbladh@gmail.com>
2024-02-05 14:31:11 +0100

    Add skeleton for CI server

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,11 @@ repositories {
 }
 
 dependencies {
+	// https://mvnrepository.com/artifact/javax.servlet/javax.servlet-api
+	compileOnly("javax.servlet:javax.servlet-api:3.0.1")
+	// https://mvnrepository.com/artifact/org.eclipse.jetty/jetty-servlet
+	implementation("org.eclipse.jetty:jetty-servlet:7.0.2.v20100331")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.25.1")
