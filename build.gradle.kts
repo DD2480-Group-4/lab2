a851b3a498aa16c3e6895dce5543c660b26e74d1
Robin Claesson <robcla@kth.se>
2024-02-06 14:47:24 +0100

    Replaced spaces with tab

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
 	// https://mvnrepository.com/artifact/org.eclipse.jetty/jetty-servlet
 	implementation("org.eclipse.jetty:jetty-servlet:7.0.2.v20100331")
 
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.8.9")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.8.9")
 
 	testImplementation(platform("org.junit:junit-bom:5.9.1"))
 	testImplementation("org.junit.jupiter:junit-jupiter")
