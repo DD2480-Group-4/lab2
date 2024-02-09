729ca913a0d9e62cb3e59fc5ad24c07597cad451
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2024-02-09 14:39:36 +0100

    feat: Implement Gradle Builder (#16)

--- src/main/java/ci/Builder.java
+++ src/main/java/ci/Builder.java
@@ -1,4 +1,86 @@
 package ci;
 
-public class Builder {
+import org.apache.commons.io.FileUtils;
+import org.gradle.tooling.BuildException;
+import org.gradle.tooling.BuildLauncher;
+import org.gradle.tooling.GradleConnector;
+import org.gradle.tooling.ResultHandler;
+import org.gradle.tooling.internal.consumer.BlockingResultHandler;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+
+/**
+ * Builder that can run gradle actions on projects.
+ * Create a new builder with {@link Builder#Builder(Path)}} and run
+ * {@link Builder#runTasks(Consumer, ResultHandler)}.
+ * For example:
+ * try (var builder = new Builder()) {
+ *      builder.runTasks(
+ *          launcher -> launcher.forTasks("build"),
+ *          new BlockingResultHandler<>(Void.class)
+ *      );
+ * } catch (IOException err) {
+ *     throw new RuntimeException(err);
+ * }
+ */
+public class Builder implements AutoCloseable {
+
+	private final GradleConnector connector;
+	private final Path projectDir;
+
+	/**
+	 * Creates a new builder.
+	 * @param projectDir The directory of the project to build.
+	 */
+	public Builder(Path projectDir) {
+		this.projectDir = projectDir;
+		connector = GradleConnector.newConnector();
+		connector.forProjectDirectory(projectDir.toFile());
+	}
+
+	/**
+	 * Run gradle tasks on the project.
+	 * Example invocation:
+	 * builder.runTasks(
+	 *     launcher -> launcher.forTasks("build"),
+	 *     new BlockingResultHandler<>(Void.class)
+	 * );
+	 * @param executeTasks Allows you to run tasks on the {@link BuildLauncher}.
+	 * @param handler The {@link ResultHandler}, allows you to get the result from the compilation.
+	 */
+	public void runTasks(Consumer<BuildLauncher> executeTasks, ResultHandler<? super Void> handler) {
+		try (var connection = connector.connect()) {
+			var taskRunner = connection.newBuild();
+			executeTasks.accept(taskRunner);
+			taskRunner.run(handler);
+		}
+	}
+
+	/**
+	 * Removes gradle-specific files.
+	 * @throws IOException If it is unable to remove the files.
+	 */
+	@Override
+	public void close() throws IOException {
+		FileUtils.deleteDirectory(projectDir.resolve(".gradle").toFile());
+		FileUtils.deleteDirectory(projectDir.resolve("build").toFile());
+	}
+
+	/**
+	 * Try to compile the project.
+	 * TODO Run tests also
+	 * @return {@link CommitStatuses#failure} if the build fails, {@link CommitStatuses#success} otherwise.
+	 */
+	public CommitStatuses build() {
+		var handler = new BlockingResultHandler<>(Void.class);
+		try {
+			runTasks(launcher -> launcher.forTasks("build"), handler);
+			handler.getResult();
+			return CommitStatuses.success;
+		} catch (BuildException ignored) {
+			return CommitStatuses.failure;
+		}
+	}
 }
--- src/test/java/ci/BuilderTest.java
+++ src/test/java/ci/BuilderTest.java
@@ -4,5 +4,45 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class BuilderTest {
+
+	/**
+	 * BuilderTest:
+	 * Attempts to build a Gradle project.
+	 * The project is expected to compile successfully.
+	 */
+	@Test
+	@DisplayName("Self-build test")
+	void buildProject() {
+		var buildDir = Path.of("./src/test/resources/build_success");
+		try (var builder = new Builder(buildDir)) {
+			Assertions.assertThat(builder.build()).isEqualTo(CommitStatuses.success);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		Assertions.assertThat(buildDir.resolve(".gradle").toFile().exists()).isFalse();
+		Assertions.assertThat(buildDir.resolve("build").toFile().exists()).isFalse();
+	}
+
+	/**
+	 * BuilderTest:
+	 * Attempts to compile a Gradle project with a syntax error.
+	 * The compilation is expected to fail.
+	 */
+	@Test
+	@DisplayName("Self-build failure")
+	void buildFail() {
+		var buildDir = Path.of("./src/test/resources/build_fail");
+		try (var builder = new Builder(buildDir)) {
+			Assertions.assertThat(builder.build()).isEqualTo(CommitStatuses.failure);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		Assertions.assertThat(buildDir.resolve(".gradle").toFile().exists()).isFalse();
+		Assertions.assertThat(buildDir.resolve("build").toFile().exists()).isFalse();
+	}
+
 }
