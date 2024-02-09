cae6d300498371fc3d377b6ba9f3d8b3b1f6aa01
Robin Claesson <robcla@kth.se>
2024-02-09 13:52:47 +0100

    Added sql insert functions

--- src/main/java/ci/HistoryDAO.java
+++ src/main/java/ci/HistoryDAO.java
@@ -3,6 +3,8 @@ package ci;
 import java.io.File;
 import java.sql.*;
 
+import ci.PushPayload.Commit;
+
 public class HistoryDAO {
 
 	private final String databaseName;
@@ -10,6 +12,7 @@ public class HistoryDAO {
 	public HistoryDAO(String databaseName) throws SQLException {
 		this.databaseName = databaseName;
 
+		//Build the database if it does not exist
 		File databaseFile = new File(databaseName);
 		if (!databaseFile.exists()) {
 
@@ -30,9 +33,10 @@ public class HistoryDAO {
 			statement.executeBatch();
 			connection.close();
 		}
+
 	}
 
-	public Connection getConnection() throws SQLException {
+	private Connection getConnection() throws SQLException {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			return DriverManager.getConnection("jdbc:sqlite:" + databaseName);
@@ -42,4 +46,67 @@ public class HistoryDAO {
 		}
 	}
 
+	public int addAuthor(String name, String username, String email) throws SQLException {
+
+		Connection connection = getConnection();
+
+		//Check if the author already exists
+		PreparedStatement getStatement = connection.prepareStatement("SELECT id FROM authors WHERE email = ?");
+		getStatement.setString(1, email);
+		ResultSet rs = getStatement.executeQuery();
+		if (rs.next()) {
+			connection.close();
+			return rs.getInt("id");
+		}
+
+		//Insert the author
+		PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO authors (name, username, email) VALUES (?, ?, ?)");
+		insertStatement.setString(1, name);
+		insertStatement.setString(2, username);
+		insertStatement.setString(3, email);
+		insertStatement.execute();
+
+		rs = insertStatement.getGeneratedKeys();
+		int id = rs.getInt(1);
+		connection.close();
+		return id;
+	}
+
+	public int addSender(String login, String url, String avatarUrl) throws SQLException {
+
+		Connection connection = getConnection();
+
+		//Check if the sender already exists
+		PreparedStatement getStatement = connection.prepareStatement("SELECT id FROM senders WHERE login = ?");
+		getStatement.setString(1, login);
+		ResultSet rs = getStatement.executeQuery();
+		if (rs.next()) {
+			connection.close();
+			return rs.getInt("id");
+		}
+
+		//Insert the sender
+		PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO senders (login, url, avatar_url) VALUES (?, ?, ?)");
+		insertStatement.setString(1, login);
+		insertStatement.setString(2, url);
+		insertStatement.setString(3, avatarUrl);
+		insertStatement.execute();
+
+		rs = insertStatement.getGeneratedKeys();
+		int id = rs.getInt(1);
+		connection.close();
+		return id;
+	}
+
+	// public int addCommit(Commit commit) throws SQLException {
+
+	// 	Connection connection = getConnection();
+
+	// 	//Check if the commit already exists
+	// 	PreparedStatement getStatement = connection.prepareStatement("SELECT id FROM commits WHERE sha = ?");
+	// 	ResultSet rs = getStatement.executeQuery();
+
+
+		
+	// }
 }
