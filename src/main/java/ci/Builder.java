801ee002b0b6794f424a7ef427adaae7441715e8
Douglas Fischer <34154299+DouglasFischer@users.noreply.github.com>
2024-02-12 14:25:28 +0100

    feat: Implement Clone Target Repository (#17)
    Co-authored-by: Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>

--- src/main/java/ci/Builder.java
+++ src/main/java/ci/Builder.java
@@ -1,12 +1,16 @@
 package ci;
 
 import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.CreateBranchCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.gradle.tooling.BuildException;
 import org.gradle.tooling.BuildLauncher;
 import org.gradle.tooling.GradleConnector;
 import org.gradle.tooling.ResultHandler;
 import org.gradle.tooling.internal.consumer.BlockingResultHandler;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.function.Consumer;
@@ -81,6 +85,84 @@ public class Builder implements AutoCloseable {
 			return CommitStatuses.success;
 		} catch (BuildException ignored) {
 			return CommitStatuses.failure;
+		}
+	}
+
+	private boolean deleteAfter = false;
+	/**
+	 * Clones repository using provided Url into temporary directory
+	 * Checks out the provided branch
+	 * Deletes the temporary directory at the end
+	 *
+	 * @param targetRepoUrl Url to repository to clone
+	 * @param targetBranch Branch from repository to be checked out
+	 */
+	public void cloneTargetRepo(String targetRepoUrl, String targetBranch) throws GitAPIException {
+		Git git;
+
+		File dir = projectDir.toFile();
+		// Deletes the temp directory if it already exists, then creates a new temp dir
+		deleteDirectory(dir);
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+
+		try {
+			// Clone
+			System.out.println("Cloning " + targetRepoUrl + " into " + dir);
+			git = Git.cloneRepository()
+				.setURI(targetRepoUrl)
+				.setDirectory(dir)
+				.call();
+			System.out.println("Completed Cloning");
+		} catch (GitAPIException e) {
+			System.out.println("Exception occurred while cloning repository");
+			e.printStackTrace();
+			return;
+		}
+
+		try {
+			// Checkout branch
+			System.out.println("Checking out branch " + targetBranch + " of repo " + targetRepoUrl);
+			git.branchCreate()
+				.setName(targetBranch)
+				.setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.TRACK)
+				.setStartPoint("origin/" + targetBranch)
+				.call();
+			git.checkout()
+				.setName(targetBranch)
+				.call();
+			System.out.println("Completed Branch-Checkout");
+		} catch (GitAPIException e) {
+			System.out.println("Exception occurred while checking out branch");
+			e.printStackTrace();
+		} finally {
+			// Close the Git repository to release resources
+			if (git != null && git.getRepository() != null) {
+				git.getRepository().close();
+			}
+		}
+
+		// Delete the temp directory if needed
+		if (deleteAfter) {
+			deleteDirectory(dir);
+		}
+	}
+
+	/**
+	 * Deletes a given directory if it exists
+	 *
+	 * @param directory Directory to be deleted
+	 */
+	public static void deleteDirectory(File directory) {
+		if (directory.exists()) {
+			try {
+				FileUtils.deleteDirectory(directory);
+				System.out.println("Temp directory deleted");
+			} catch (IOException e) {
+				System.out.println("Exception occurred while deleting temp directory");
+				e.printStackTrace();
+			}
 		}
 	}
 }
--- src/test/java/ci/BuilderTest.java
+++ src/test/java/ci/BuilderTest.java
@@ -1,13 +1,123 @@
 package ci;
 
 import org.assertj.core.api.Assertions;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static org.assertj.core.api.Fail.fail;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BuilderTest {
+
+	/*
+	 * Remote Directory Cloning Test:
+	 * Creates a new remote directory containing a master file.
+	 * Creates a new secondary branch containing a secondary file
+	 * Runs the cloneTargetRepo function on the remote directory's secondary branch.
+	 * Both files and are expected to exist in the local directory.
+	 * Both directories are deleted after the test.
+	 */
+	@Test
+	@DisplayName("Test Repository Cloning")
+	void testRepositoryCloning() throws GitAPIException, IOException {
+
+		String currentDir = System.getProperty("user.dir");
+		String testRemotePath = currentDir.concat("/remotetest");
+		String dirPath = currentDir.concat("/temp");
+
+		File testRemote = new File(testRemotePath);
+		File testDir = new File(dirPath);
+
+		// Creates the remote dir
+		testRemote.mkdirs();
+
+		try (Builder builder = new Builder(Path.of(dirPath))) {
+
+			Git git = Git.init().setDirectory(testRemote).call();
+			// Create a file in the master branch of the repository
+			File masterFippelFileRemote = new File(testRemote, "fippel_master.txt");
+			Files.write(masterFippelFileRemote.toPath(), "Hello, master!".getBytes());
+			git.add().addFilepattern(".").call();
+			git.commit().setMessage("Initial commit").call();
+			//git.push().call();
+
+			// Create and checkout the secondary branch
+			git.branchCreate().setName("secondary").call();
+			git.checkout().setName("secondary").call();
+
+			// Create a file in the secondary branch of the repository
+			File secondaryFippelFileRemote = new File(testRemote, "fippel_secondary.txt");
+			Files.write(secondaryFippelFileRemote.toPath(), "Hello, secondary!".getBytes());
+			git.add().addFilepattern(".").call();
+			git.commit().setMessage("Secondary commit").call();
+			//git.push().call();
+
+			// Checkout master
+			git.checkout().setName("master").call();
+
+			builder.cloneTargetRepo(testRemotePath, "secondary");
+
+			File masterFippelFilePath = new File(dirPath.concat("/fippel_master.txt"));
+			File secondaryFippelFilePath = new File(dirPath.concat("/fippel_secondary.txt"));
+
+			String secondaryContent = new String(Files.readAllBytes(secondaryFippelFilePath.toPath()));
+
+			assertTrue(masterFippelFilePath.exists());
+			assertTrue(secondaryFippelFilePath.exists());
+
+			assertEquals("Hello, secondary!", secondaryContent);
+
+		} catch (GitAPIException | IOException e) {
+			// handle the exception or fail the test
+			e.printStackTrace();
+			fail("Exception occurred during the test: " + e.getMessage());
+		} finally {
+			Builder.deleteDirectory(testRemote);
+			Builder.deleteDirectory(testDir);
+		}
+	}
+
+	/*
+	 * Directory Deletion Test:
+	 * Creates a new directory containing a file.
+	 * Runs the deleteDirectory function on the directory.
+	 * Both file and directory are expected to be deleted.
+	 */
+	@Test
+	@DisplayName("Check file deletion")
+	void testDeleteDirectory() {
+		String currentDir = System.getProperty("user.dir");
+		String testDirPath = currentDir.concat("/temptest");
+
+		File testDir = new File(testDirPath);
+		// Creates a new temp dir
+		testDir.mkdirs();
+
+		// Creates a file in the temporary test directory
+		File testFile = new File(testDir, "fippel.txt");
+
+		try {
+			testFile.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		assertTrue(testDir.exists());
+		assertTrue(testFile.exists());
+
+		Builder.deleteDirectory(testDir);
+
+		assertFalse(testDir.exists());
+		assertFalse(testFile.exists());
+	}
+
 
 	/**
 	 * BuilderTest:
