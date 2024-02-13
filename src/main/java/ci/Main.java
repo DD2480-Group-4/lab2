93a728af4e19f3bc5b7462cff9fddc49c3ec50ca
Robin Claesson <robcla@kth.se>
2024-02-13 15:14:24 +0100

    Parsed test results

--- src/main/java/ci/Main.java
+++ src/main/java/ci/Main.java
@@ -15,31 +15,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.io.OutputStream;
 import java.net.http.HttpClient;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 /**
  * Main executable for Continuous-Integration handler
  */
-public class Main extends AbstractHandler
-{
+public class Main extends AbstractHandler {
+	private final Pattern testResultPattern = Pattern.compile("<div class=\"counter\">[0-9]+</div>");
 
 	@Override
 	public void handle(
-		String target,
-		Request baseRequest,
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws IOException, ServletException {
+			String target,
+			Request baseRequest,
+			HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
 		response.setContentType("text/html;charset=utf-8");
 		response.setStatus(HttpServletResponse.SC_OK);
 		baseRequest.setHandled(true);
@@ -75,7 +78,7 @@ public class Main extends AbstractHandler
 			TeeOutputStream buildAndStdOut = new TeeOutputStream(buildOutput, System.out);
 			TeeOutputStream testAndStdOut = new TeeOutputStream(testOutput, System.out);
 
-			try(var builder = createBuilder(buildPath, buildAndStdOut, testAndStdOut)) {
+			try (var builder = createBuilder(buildPath, buildAndStdOut, testAndStdOut)) {
 				builder.cloneTargetRepo(payload.getCloneUrl(), payload.getBranch());
 				var result = builder.buildAndTest();
 				var desc = switch (result) {
@@ -87,10 +90,20 @@ public class Main extends AbstractHandler
 				notifier.setCommitStatus(result, desc, accessUrl);
 
 				BuildDetails buildDetails = new BuildDetails(result.ordinal(), buildOutput.toString());
-				TestDetails testDetails = new TestDetails(0, 0, testOutput.toString());
+
+				String testResultFile = Files.readString(buildPath.resolve("build/reports/tests/test/index.html"));
+
+				Matcher matcher = testResultPattern.matcher(testResultFile);
+
+				matcher.find();
+				int totalTests = Integer.parseInt(matcher.group().split(">")[1].split("<")[0]);
+				matcher.find();
+				int failedTests = Integer.parseInt(matcher.group().split(">")[1].split("<")[0]);
+
+				TestDetails testDetails = new TestDetails(totalTests, totalTests - failedTests, testOutput.toString());
 				BuildInfo buildInfo = new BuildInfo(payload.getSender(),
-													Arrays.asList(payload.getCommits()),
-													buildDetails, testDetails, payload.getPushedAt());
+						Arrays.asList(payload.getCommits()),
+						buildDetails, testDetails, payload.getPushedAt());
 
 				historyDAO.addHistory(buildInfo);
 			} catch (GitAPIException err) {
@@ -119,7 +132,8 @@ public class Main extends AbstractHandler
 					// Display links to all builds from history
 					if (!target.startsWith("/build_")) {
 						for (BuildInfo build : history) {
-							response.getWriter().println("<a href=\"/build_" + build.getId() + "/\">Build Info " + build.getId() + "</a><br>");
+							response.getWriter().println("<a href=\"/build_" + build.getId() + "/\">Build Info "
+									+ build.getId() + "</a><br>");
 						}
 					}
 					// Display build info for specific build if link is clicked
@@ -145,6 +159,7 @@ public class Main extends AbstractHandler
 	/**
 	 * Creates a new notifier from a PushPayload.
 	 * This function merely exists to allow mock-testing.
+	 * 
 	 * @param payload The payload to use in the notifier.
 	 * @return The notifier.
 	 */
@@ -155,7 +170,8 @@ public class Main extends AbstractHandler
 	/**
 	 * Creates a new builder from a path and an outputstream.
 	 * This function merely exists to allow mock-testing.
-	 * @param path The file path to build the project at.
+	 * 
+	 * @param path   The file path to build the project at.
 	 * @param output The output stream to print logs to.
 	 * @return The builder.
 	 */
@@ -166,15 +182,17 @@ public class Main extends AbstractHandler
 	/**
 	 * Creates a new HistoryDAO object.
 	 * This function merely exists to allow mock-testing.
+	 * 
 	 * @param dbPath The path to the database file.
 	 * @return The HistoryDAO.
 	 */
 	protected HistoryDAO createHistoryDAO(String dbPath) throws SQLException {
 		return new HistoryDAO(dbPath);
 	}
-	
+
 	/**
-	 * Prints information about PushPayload object received from GitHub webhook to standard output
+	 * Prints information about PushPayload object received from GitHub webhook to
+	 * standard output
 	 *
 	 * @param payload Parsed payload object
 	 */
@@ -192,50 +210,64 @@ public class Main extends AbstractHandler
 			sb.append("Author: ").append(commit.author().name());
 			sb.append(" | Message: ").append(commit.message()).append("\n");
 		}
-		
+
 		System.out.println(sb.toString());
 	}
- 
+
 	/**
-	 * Initialize CI server 
+	 * Initialize CI server
 	 */
 	public static void main(String[] args) throws Exception {
 		Server server = new Server(8080);
 		server.setHandler(new Main());
 		server.start();
 		server.join();
-/*
-		HistoryDAO dao = new HistoryDAO("builds.db");
-
-		Connection connection = dao.getConnection();
-		Statement statement = connection.createStatement();
-
-		statement
-				.addBatch("INSERT INTO authors (name, username, email) VALUES ('John Doe', 'johndoe', 'john@doe.com')");
-		statement.addBatch(
-				"INSERT INTO authors (name, username, email) VALUES ('Mr Bean', 'mrbean', 'beanAndTeddy@funny.org')");
-
-		statement.addBatch(
-				"INSERT INTO commits (sha, message, authorId, url, modifiedFiles) VALUES ('sha1', 'Commit1', 1, 'commitUrl1', 'File1.txt,File2.txt')");
-		statement.addBatch(
-				"INSERT INTO commits (sha, message, authorId, url, modifiedFiles) VALUES ('sha2', 'Commit2', 2, 'commitUrl2', 'File1.txt,File2.txt')");
-		statement.addBatch(
-				"INSERT INTO commits (sha, message, authorId, url, modifiedFiles) VALUES ('sha3', 'Commit3', 1, 'commitUrl3', 'File2.txt')");
-
-		statement.addBatch(
-				"INSERT INTO senders (login, url, avatarUrl) VALUES ('johndoe', 'johndoeUrl', 'johndoeAvatarUrl')");
-
-		statement.addBatch(
-				"INSERT INTO history ('senderId', 'buildResult', 'buildLog', 'totalTests', 'numOfPassedTests', 'testLog', 'buildDate') VALUES (1, 1, 'Build Log 1', 10, 10, 'All test passed', '2021-01-01T00:00:00')");
-		statement.addBatch(
-				"INSERT INTO history ('senderId', 'buildResult', 'buildLog', 'totalTests', 'numOfPassedTests', 'testLog', buildDate) VALUES (1, 0, 'Build Log 2', 10, 5, '5 test failed', '2021-01-02T00:00:00')");
-
-		statement.addBatch("INSERT INTO historyCommits ('historyId', 'commitId') VALUES (1, 1)");
-		statement.addBatch("INSERT INTO historyCommits ('historyId', 'commitId') VALUES (1, 2)");
-		statement.addBatch("INSERT INTO historyCommits ('historyId', 'commitId') VALUES (2, 3)");
-
-		statement.executeBatch();
-		*/
+		/*
+		 * HistoryDAO dao = new HistoryDAO("builds.db");
+		 * 
+		 * Connection connection = dao.getConnection();
+		 * Statement statement = connection.createStatement();
+		 * 
+		 * statement
+		 * .addBatch("INSERT INTO authors (name, username, email) VALUES ('John Doe', 'johndoe', 'john@doe.com')"
+		 * );
+		 * statement.addBatch(
+		 * "INSERT INTO authors (name, username, email) VALUES ('Mr Bean', 'mrbean', 'beanAndTeddy@funny.org')"
+		 * );
+		 * 
+		 * statement.addBatch(
+		 * "INSERT INTO commits (sha, message, authorId, url, modifiedFiles) VALUES ('sha1', 'Commit1', 1, 'commitUrl1', 'File1.txt,File2.txt')"
+		 * );
+		 * statement.addBatch(
+		 * "INSERT INTO commits (sha, message, authorId, url, modifiedFiles) VALUES ('sha2', 'Commit2', 2, 'commitUrl2', 'File1.txt,File2.txt')"
+		 * );
+		 * statement.addBatch(
+		 * "INSERT INTO commits (sha, message, authorId, url, modifiedFiles) VALUES ('sha3', 'Commit3', 1, 'commitUrl3', 'File2.txt')"
+		 * );
+		 * 
+		 * statement.addBatch(
+		 * "INSERT INTO senders (login, url, avatarUrl) VALUES ('johndoe', 'johndoeUrl', 'johndoeAvatarUrl')"
+		 * );
+		 * 
+		 * statement.addBatch(
+		 * "INSERT INTO history ('senderId', 'buildResult', 'buildLog', 'totalTests', 'numOfPassedTests', 'testLog', 'buildDate') VALUES (1, 1, 'Build Log 1', 10, 10, 'All test passed', '2021-01-01T00:00:00')"
+		 * );
+		 * statement.addBatch(
+		 * "INSERT INTO history ('senderId', 'buildResult', 'buildLog', 'totalTests', 'numOfPassedTests', 'testLog', buildDate) VALUES (1, 0, 'Build Log 2', 10, 5, '5 test failed', '2021-01-02T00:00:00')"
+		 * );
+		 * 
+		 * statement.
+		 * addBatch("INSERT INTO historyCommits ('historyId', 'commitId') VALUES (1, 1)"
+		 * );
+		 * statement.
+		 * addBatch("INSERT INTO historyCommits ('historyId', 'commitId') VALUES (1, 2)"
+		 * );
+		 * statement.
+		 * addBatch("INSERT INTO historyCommits ('historyId', 'commitId') VALUES (2, 3)"
+		 * );
+		 * 
+		 * statement.executeBatch();
+		 */
 
 	}
 
